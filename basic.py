8669054575eab9c0d56d828f460d33340028d98d
delroy <delroygayle01@gmail.com>
2024-09-14 20:30:29 +0100

    Disallow NOT operator being used with strings - now shows Illegal Operation

--- basic.py
+++ basic.py
@@ -1587,7 +1587,7 @@ class Value:
     def ored_by(self, other):
         return None, self.illegal_operation(other)
 
-    def notted(self, other):
+    def notted(self, other=None):
         return None, self.illegal_operation(other)
 
     def execute(self, args):
